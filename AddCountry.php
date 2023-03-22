5fc474d47b9d60e5406fc48ae18c9e7c54064ce5
Martin Harrer <martin.harrer@fh-hagenberg.at>
2023-03-22 15:23:07 +0100

    added Exception Handling

--- AddCountry.php
+++ AddCountry.php
@@ -30,5 +30,22 @@ require_once 'error_handling.php';
      * Add methods and properties as needed to AddCountry.php to store and retrieve the data sent bei index.html
      */
 
+try {
+// TODO Erzeugen Sie ein erstes Objekt der Klasse
+// TODO Benennen sie das Objekt passend zur Klasse und zu PSR1 in camelCase.
+//    $addCountry = new AddCountry();
 
+//    $addCountry->Show();
+
+
+// TODO Call StoreAndRetrieve() and handle PDOExceptions here
+//    $addCountry->StoreAndRetrieve();
+} catch (Exception $e) {
+    echo "<h1>Error Page for Debugging</h1>.";
+    echo "<p><strong>Don't use that in a production environment!</strong></p>";
+    echo "<p>There is an error in " . $e->getFile() . " on line " . $e->getLine() . ".</p>";
+    echo "<p>Message: " . $e->getMessage() . "</p>";
+    echo "<p>Code: " . $e->getCode() . "</p>";
+    echo "<p>Trace: " . $e->getTraceAsString() . "</p>";
+}
 // At the end of a PHP file there is only a line break and no closing PHP processing instruction according to PSR2
